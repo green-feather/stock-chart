e7120c628e346a1302fc321957f4b6fce18cfc1e
Tyler Lindow <tyler.lindow@gmail.com>
2019-04-20 12:22:59 -0700

    Define postgresql schemas for each table

---
+++ database/stockMain/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE stockMain (                                                                       
+  id varchar(10),
+  stockId varchar(10),
+  averageStock numeric(5,2),
+  changePercent numeric(4,2)
+);
