2f0b292ee0ad758388e9204a7ada79c00af15f63
Tyler Lindow <tyler.lindow@gmail.com>
2019-04-20 10:39:11 -0700

    Init file for shared data generation methods

---
+++ database/generationMethods.js
@@ -0,0 +1,50 @@
+const faker = require('faker');
+
+module.exports = {
+  
+  // Generate stock chart data based on schema
+  generateData: (index, numberOfEntries) => {
+    return `${module.exports.numberToPaddedString(numberOfEntries)},${index},
+            ${module.exports.generateDataPoints(
+              module.exports.generateInBetween(50, 200).toFixed(2)
+              )}\n`;
+  },
+
+  // Creates random data points that are 5-25 points above or below previous data point
+  generateDataPoints: (init) => {
+    return Math.abs(module.exports.generateInBetween(
+      init - module.exports.generateInBetween(5, 25, 'interger'),
+      init + module.exports.generateInBetween(5, 25, 'interger')).toFixed(2)
+    );
+  },
+
+  // Creates a random number between min & max
+  generateInBetween: (min, max, type) => {
+    if (type === 'interger') {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min)) + min; 
+    } else {
+      return (Math.random() * (max - min)) + min; 
+    }
+  },
+  
+  // Creates fake stock tag names
+  generateTags: (number) => {
+    number = number || 3;
+    let returnArr = [];
+    for (let i = 0; i < 5; i++) {
+      if (i > number) {
+        returnArr.push(null);
+        continue;
+      }
+      returnArr.push(faker.fake("{{commerce.department}}"));
+    }
+    return returnArr;
+  },
+  
+  // Creates a sring number padded with zeros up to ten digits
+  numberToPaddedString: (number) => {
+    return number.toString().padStart(10, '0');
+  }
+}
