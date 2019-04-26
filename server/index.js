63e76358221c99fd32335f327aee70c0a33f284d
Tyler Lindow <tyler.lindow@gmail.com>
2019-04-25 19:19:24 -0700

    Add GET route and data formating for one stockId

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 const cors = require('cors');
-const Stocks = require('../database/StockChart.js');
+const client = require('../database/index');
+const { getOne, updateOne, postOne, deleteOne } = require('../database/StockChart.js');
 
 const app = express();
 const port = 4000;
@@ -17,30 +19,43 @@ app.listen(port, () => {
   console.log(`Server is now listening on port: ${port}`)
 })
 
-app.get('/api/:stockId', (req, res) => {
+app.get('/api/:stockId', async (req, res) => {
   console.log('Got a request searching for', req.params.stockId);
-  Stocks.find({stockId: req.params.stockId}, (err, data) => {
-    if (err) {
-      console.log(err.message);
-    } else if (!data.length) {
-      Stocks.find({id: req.params.stockId}, (err, data) => {
-        if (err) {
-          console.log(err.message);
-        } else if (!data.length) {
-          console.log('Data not found');
-          res.sendStatus(404);
-        } else {
-          console.log(`Sending ${req.params.stockId} to client`);
-          res.send(data);
-        }
-      }) 
-    } else {
-      console.log(`Sending ${req.params.stockId} to client`);
-      res.send(data);
+  const data = await getOne(req.params.stockId);
+
+  ///// Reformat response data to work with client-side ////////
+  let relatedTags = [data.relatedtags_1, data.relatedtags_2, 
+    data.relatedtags_3, data.relatedtags_4, data.relatedtags_5];
+
+  //Splice empty tags
+  for (let i = 0; i < relatedTags.length; i += 1) {
+    if (relatedTags[i].length === 0) {
+      relatedTags.splice(i, 1);
+      i -= 1;
     }
-  }) 
+  }
+  const [stockCompany, noOfOwners, recommendationPercent] = 
+    [data.stockcompany, data.noofowners, data.recommendationpercent];
+  const stockInfo = 
+    {relatedTags, stockCompany, noOfOwners, recommendationPercent}
+  const [day, week, month, threeMonth, year, fiveYear] =
+    [data.prices_day.split('|'), data.prices_week.split('|'),
+    data.prices_month.split('|'),data.prices_threemonth.split('|'),
+    data.prices_year.split('|'), data.prices_fiveyear.split('|')];
+  const stockData = {day, week, month, threeMonth, year, fiveYear};
+  const [id, stockId, averageStock, changePercent] = 
+    [data.id, data.stockid, data.averagestock, data.changepercent]
+  const stock = _.extend(
+    {id, stockId, averageStock, changePercent}, 
+    {stockInfo}, 
+    {stockData}
+  );
+  ///////////////////// end of reformat ////////////////////////
+
+  // Send data to client-side and disconnect from database
+  res.send([stock]);
 })
 
 app.get('/:stockId', (req, res) => {
   res.sendFile(path.join(__dirname, '/../public/dist/index.html'));
-})
+});
